c990d7c9ea875f277a305686f531b15f5528bfa7
lankton <lankton2015@gmail.com>
2016-05-19 19:49:29 +0800

    初始化项目

---
+++ app/src/main/java/cn/lankton/swipelinearlayout/MainActivity.java
@@ -0,0 +1,168 @@
+package cn.lankton.swipelinearlayout;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.lankton.swipelinearlayout.lib.SwipeLinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    MyAdapter adapter;
+    List<SwipeLinearLayout> swipeLinearLayouts = new ArrayList<>();
+    SwipeLinearLayout curSll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new MyAdapter(this);
+        listView.setAdapter(adapter);
+    }
+
+
+    class MyAdapter extends BaseAdapter implements SwipeLinearLayout.OnSwipeListener {
+        Context mContext;
+        LayoutInflater inflater;
+        public MyAdapter(Context context) {
+            mContext = context;
+            inflater = LayoutInflater.from(mContext);
+        }
+
+        @Override
+        public int getCount() {
+            return 23;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (null == convertView) {
+                convertView = inflater.inflate(R.layout.item_list, null);
+                holder = new ViewHolder(convertView);
+                swipeLinearLayouts.add(holder.sll);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            // 滚到收缩状态
+            holder.sll.scrollTo(0,0);
+            final ViewHolder finalHolder = holder;
+            holder.ll_left.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "click item " + position, Toast.LENGTH_SHORT).show();
+                    finalHolder.sll.scrollAuto(SwipeLinearLayout.DIRECTION_SHRINK);
+                }
+            });
+            holder.tv_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "delete item " + position, Toast.LENGTH_SHORT).show();
+                    finalHolder.sll.scrollAuto(SwipeLinearLayout.DIRECTION_SHRINK);
+                }
+            });
+            return convertView;
+        }
+
+
+        @Override
+        public void onBeginScroll(SwipeLinearLayout thisSll, boolean isHorizontal) {
+            if (false == isHorizontal) {
+                // 可以理解为sll丢失了事件，后续都会被上层拦截
+                for (SwipeLinearLayout sll : swipeLinearLayouts) {
+                    if (null == sll) {
+                        continue;
+                    }
+                    sll.scrollAuto(SwipeLinearLayout.DIRECTION_SHRINK);
+                    if (curSll == thisSll) {
+                        sll.setDragable(true);
+                    }
+                }
+                curSll = null;
+            } else {
+                for (SwipeLinearLayout sll : swipeLinearLayouts) {
+                    if (null == sll) {
+                        continue;
+                    }
+                    if (!sll.equals(thisSll)) {
+                        //划开一个sll， 其他收缩
+                        sll.scrollAuto(SwipeLinearLayout.DIRECTION_SHRINK);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onTouchUp(SwipeLinearLayout thisSll) {
+            Log.v("lanktondebug", "up");
+            if (curSll == thisSll) {
+                // 某个sll结束拖动(可能并未复位)， 其他sll可以侧滑
+                for (SwipeLinearLayout sll : swipeLinearLayouts) {
+                    if (null == sll) {
+                        continue;
+                    }
+                    if (!sll.equals(thisSll)) {
+                        sll.setDragable(true);
+                    }
+                }
+                curSll = null;
+            }
+        }
+
+        @Override
+        public void onTouchDown(SwipeLinearLayout thisSll) {
+            Log.v("lanktondebug", "down");
+            if (curSll == null) {
+                curSll = thisSll;
+                Log.v("lanktondebug", "false all");
+                for (SwipeLinearLayout sll : swipeLinearLayouts) {
+                    if (null == sll) {
+                        continue;
+                    }
+                    if (!sll.equals(thisSll)) {
+                        sll.setDragable(false);
+                    }
+                }
+            }
+        }
+
+        class ViewHolder {
+            SwipeLinearLayout sll;
+            View ll_left;
+            View tv_delete;
+
+            public ViewHolder(View v) {
+                sll = (SwipeLinearLayout) v.findViewById(R.id.sll);
+                ll_left = v.findViewById(R.id.ll_left);
+                tv_delete = v.findViewById(R.id.tv_delete);
+                sll.setOnSwipeListener(MyAdapter.this);
+            }
+
+        }
+    }
+
+
+}
